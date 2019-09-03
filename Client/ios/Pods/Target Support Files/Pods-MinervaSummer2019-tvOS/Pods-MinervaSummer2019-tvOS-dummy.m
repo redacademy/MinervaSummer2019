99c04d009a81ffc7ad8cbd2bb72468b4a5277468
Oli Wright <oli.wright@blueyonder.co.uk>
2019-09-03 10:49:41 -0700

    changed folder structure

---
+++ Client/ios/Pods/Target Support Files/Pods-MinervaSummer2019-tvOS/Pods-MinervaSummer2019-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MinervaSummer2019_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_MinervaSummer2019_tvOS
+@end
---
+++ Client/ios/Pods/Target Support Files/Pods-MinervaSummer2019/Pods-MinervaSummer2019-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MinervaSummer2019 : NSObject
+@end
+@implementation PodsDummy_Pods_MinervaSummer2019
+@end
