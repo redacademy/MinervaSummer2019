b36e4b7c912905fd136f7210f9fca527c81d22f7
Oli Wright <oli.wright@blueyonder.co.uk>
2019-09-03 17:03:28 -0700

    removed comments, renamed method to check user token

--- client/js/components/AuthLoadingScreen/AuthLoadingScreen.js
+++ client/js/components/AuthLoadingScreen/AuthLoadingScreen.js
@@ -5,19 +5,15 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 class AuthLoadingScreen extends React.Component {
   componentDidMount() {
-    this._bootstrapAsync();
+    this.hasUserLoggedIn();
   }
 
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
+  hasUserLoggedIn = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
 
-    // This will switch to the App screen or Auth screen and this loading
-    // screen will be unmounted and thrown away.
     this.props.navigation.navigate(userToken ? 'App' : 'Auth');
   };
 
-  // Render any loading content that you like here
   render() {
     return (
       <View>
