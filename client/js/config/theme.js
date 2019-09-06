e99744dc2aa3b34c62e2848d8c6ab625a420ed84
Vincent Wang <a36168120@gmail.com>
2019-09-06 15:57:48 -0700

    add more color into theme

--- client/js/config/theme.js
+++ client/js/config/theme.js
@@ -2,7 +2,10 @@ const theme = {
   palette: {
     green: '#0DB14B',
     blue: '#00A3B4',
+    lightBlue: '#CFE3E6',
     lightGrey: '#CFCFCF',
+    mediumGrey: '#979797',
+    darkGrey: '#4D4D4F',
     red: '#D86060',
   },
 };
