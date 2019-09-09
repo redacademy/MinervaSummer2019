3b7cd8e97ac07fc1cd692ade7c2b1cc6b92b7aaf
Vincent Wang <a36168120@gmail.com>
2019-09-09 13:08:21 -0700

    commit prior to merge develop branch

--- client/js/App.js
+++ client/js/App.js
@@ -2,11 +2,12 @@ import RootStackNavigation from './navigation/RootStackNavigation';
 import React from 'react';
 import {ApolloProvider} from '@apollo/react-hooks';
 import client from './config/apollo';
-
+import CommentList from '../js/components/CommentList';
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <RootStackNavigation />
+      {/* <RootStackNavigation /> */}
+      <CommentList />
     </ApolloProvider>
   );
 };
