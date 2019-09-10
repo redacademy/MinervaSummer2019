35356878b64caba5a9b198555186dce482551134
Vincent Wang <a36168120@gmail.com>
2019-09-10 11:53:04 -0700

    commit prior editing graphcool DB

--- client/js/screens/Community/Community.js
+++ client/js/screens/Community/Community.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PostList from '../../components/PostList';
-import {ScrollView} from 'react-native';
+import {ScrollView, TouchableOpacity, View, Text} from 'react-native';
+import SelectableChips from 'react-native-chip/SelectableChips';
+import styles from './styles';
 
-const Community = ({posts}) => {
-  return (
-    <ScrollView>
-      {posts.map(post => (
-        <PostList key={post.id} post={post} />
-      ))}
-    </ScrollView>
-  );
-};
+class Community extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectPostTpoic: [],
+    };
+  }
+
+  render() {
+    const {posts} = this.props;
+
+    return (
+      <ScrollView>
+        <View style={styles.postWrapper}>
+          <TouchableOpacity opacity={0.8} style={styles.op}>
+            <Text style={styles.postInput}>Share your thoughts here...</Text>
+          </TouchableOpacity>
+
+          <Text style={styles.header}>Topics</Text>
+
+          <View style={styles.topicsWrapper}>
+            <SelectableChips
+              initialChips={['View All', 'General', 'Events', 'News']}
+              onChangeChips={chips => this.setState({selectPostTpoic: chips})}
+              chipStyle={styles.topic}
+              valueStyle={styles.topicText}
+              chipStyleSelected={styles.topicSelected}
+              valueStyleSelected={styles.topicTextSelected}
+            />
+            <View style={styles.lineB} />
+          </View>
+        </View>
+
+        {posts.map(post => (
+          <PostList key={post.id} post={post} />
+        ))}
+      </ScrollView>
+    );
+  }
+}
 
 export default Community;
--- client/js/screens/Community/styles.js
+++ client/js/screens/Community/styles.js
@@ -1,6 +1,57 @@
 import {StyleSheet} from 'react-native';
 import theme from '../../config/theme';
+import {BorderlessButton} from 'react-native-gesture-handler';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  op: {
+    borderWidth: 1,
+    borderColor: theme.palette.lightGrey,
+    height: 35,
+    borderRadius: 50,
+  },
+
+  postInput: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: '5%',
+  },
+
+  header: {
+    fontSize: 24,
+    textAlign: 'center',
+    paddingVertical: '4%',
+  },
+
+  topicsWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  topic: {
+    height: 35,
+    borderColor: theme.palette.darkGrey,
+  },
+
+  topicText: {
+    fontSize: 16,
+    color: theme.palette.darkGrey,
+  },
+
+  topicSelected: {
+    borderColor: theme.palette.blue,
+    backgroundColor: theme.palette.blue,
+  },
+
+  topicTextSelected: {
+    color: '#FFF',
+  },
+
+  lineB: {
+    width: '100%',
+    paddingTop: '4%',
+    borderBottomWidth: 1,
+    borderColor: theme.palette.lightGrey,
+  },
+});
 
 export default styles;
