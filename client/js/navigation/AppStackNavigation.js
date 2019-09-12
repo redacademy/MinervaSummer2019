bec72b0b1616c7d43a288baede493e7f1cb96b92
Vincent Wang <a36168120@gmail.com>
2019-09-12 15:19:13 -0700

    add icons for StackNavigation

--- client/js/navigation/AppStackNavigation.js
+++ client/js/navigation/AppStackNavigation.js
@@ -8,6 +8,10 @@ import CommentsScreen from '../screens/Comments';
 import ProfileScreen from '../screens/UserProfile';
 import CreatePostScreen from '../screens/CreatePost';
 
+import {View, Image} from 'react-native';
+import React from 'react';
+import styles from './styles';
+
 const CommunityStack = createStackNavigator({
   Community: {
     screen: CommunityScreen,
@@ -29,9 +33,72 @@ const ConnectionsStack = createStackNavigator({Connections: ConnectionsScreen});
 const ChatsStack = createStackNavigator({Chats: ChatsScreen});
 const ProfileStack = createStackNavigator({Profile: ProfileScreen});
 
-export default createBottomTabNavigator({
-  Community: CommunityStack,
-  Connections: ConnectionsStack,
-  Chats: ChatsStack,
-  Profile: ProfileStack,
-});
+export default createBottomTabNavigator(
+  {
+    Community: CommunityStack,
+    Connections: ConnectionsStack,
+    Chats: ChatsStack,
+    Profile: ProfileStack,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused}) => {
+        const {routeName} = navigation.state;
+        if (routeName === 'Community') {
+          return (
+            <View>
+              <Image
+                source={
+                  focused
+                    ? require('../assets/PNG/navigation_bar/community_blue.png')
+                    : require('../assets/PNG/navigation_bar/community_light_grey.png')
+                }
+              />
+            </View>
+          );
+        } else if (routeName === 'Connections') {
+          return (
+            <View>
+              <Image
+                source={
+                  focused
+                    ? require('../assets/PNG/navigation_bar/connections_blue.png')
+                    : require('../assets/PNG/navigation_bar/connections_light_grey.png')
+                }
+              />
+            </View>
+          );
+        } else if (routeName === 'Chats') {
+          return (
+            <View>
+              <Image
+                source={
+                  focused
+                    ? require('../assets/PNG/navigation_bar/chat_blue.png')
+                    : require('../assets/PNG/navigation_bar/chat_light_grey.png')
+                }
+              />
+            </View>
+          );
+        } else if (routeName === 'Profile') {
+          return (
+            <View>
+              <Image
+                source={
+                  focused
+                    ? require('../assets/PNG/navigation_bar/profile_blue.png')
+                    : require('../assets/PNG/navigation_bar/profile_light_grey.png')
+                }
+              />
+            </View>
+          );
+        }
+      },
+    }),
+    tabBarOptions: {
+      labelStyle: {
+        fontSize: 12,
+      },
+    },
+  },
+);
