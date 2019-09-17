1a42ea0993a6fcb431549b8b00f1204479899279
Oli Wright <oli.wright@blueyonder.co.uk>
2019-09-16 18:09:03 -0700

    added header styles

--- client/js/navigation/config.js
+++ client/js/navigation/config.js
@@ -4,18 +4,10 @@ import {Header} from 'react-navigation-stack';
 import LinearGradient from 'react-native-linear-gradient';
 import theme from '../config/theme';
 
-const GradientHeader = props => (
-  <View>
-    <LinearGradient
-      colors={[theme.palette.green, theme.palette.blue]}
-      start={{x: 0.0, y: 1.0}}
-      end={{x: 1.0, y: 0.0}}
-      style={[StyleSheet.absoluteFill, {height: '100%', width: '100%'}]}
-    />
-    <Header {...props} />
-  </View>
-);
-
 export const sharedNavigationOptions = () => ({
-  header: props => <GradientHeader {...props} />,
+  headerTitleStyle: {
+    fontFamily: theme.fonts.heavy,
+    color: theme.palette.blue,
+    fontSize: 20,
+  },
 });
