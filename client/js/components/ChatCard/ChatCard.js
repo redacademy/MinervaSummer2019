8a27765eecdba4e54def101ed968e50bee3f5bc4
Oli Wright <oli.wright@blueyonder.co.uk>
2019-09-16 19:46:15 -0700

    added add modal

--- client/js/components/ChatCard/ChatCard.js
+++ client/js/components/ChatCard/ChatCard.js
@@ -4,8 +4,14 @@ import styles from './styles';
 import {withNavigation} from 'react-navigation';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import theme from '../../config/theme';
-import {Mutation} from '@apollo/react-components';
-import {DELETE_CHAT, GET_USER_CHATS} from '../../config/apollo/queries';
+import {Mutation, Query} from '@apollo/react-components';
+import {
+  DELETE_CHAT,
+  GET_USERS,
+  GET_USER_CHATS,
+} from '../../config/apollo/queries';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CircularLoader from '../../components/CircularLoader';
 
 const ChatCard = ({chat, viewer, navigation}) => {
   const {members, messages} = chat;
@@ -14,6 +20,7 @@ const ChatCard = ({chat, viewer, navigation}) => {
   const recentMessage = messages[messages.length - 1];
   const [deleteModalVisible, setDeleteModalVisible] = useState(false);
   const [optionsVisible, setOptionsVisible] = useState(false);
+  const [addModalVisible, setAddModalVisible] = useState(false);
 
   return (
     <TouchableOpacity
@@ -118,6 +125,97 @@ const ChatCard = ({chat, viewer, navigation}) => {
               <Text style={styles.deleteCancel}>Cancel</Text>
             </TouchableOpacity>
           </View>
+        </View>
+      </Modal>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={addModalVisible}
+        propagateSwipe={true}>
+        <View style={styles.modalRoot}>
+          <View style={styles.backHeader}>
+            <TouchableOpacity onPress={() => setAddModalVisible(false)}>
+              <Text style={styles.backButton}>
+                <Ionicons
+                  name="ios-arrow-back"
+                  color={theme.palette.blue}
+                  size={18}
+                />
+                Back
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Query query={GET_USERS} variables={{id: viewer.id}}>
+            {({loading, error, data}) => {
+              if (loading) return <CircularLoader />;
+              if (error) return <Text>Error!{console.log(error)}</Text>;
+              return (
+                <Fragment>
+                  <View style={styles.searchWrapper}>
+                    <Text style={styles.searchLabel}>Add Members</Text>
+                    <TextInput
+                      style={styles.textInput}
+                      placeholder="Search member name..."
+                    />
+                    <Text
+                      style={[
+                        styles.subHeading,
+                        {textAlign: 'left', fontFamily: theme.fonts.heavy},
+                      ]}>
+                      Connected
+                    </Text>
+                  </View>
+                  <ScrollView style={styles.scrollRoot}>
+                    {data.allUsers.map(user => (
+                      <View key={user.id} style={styles.userCard}>
+                        <Image
+                          style={styles.image}
+                          source={require('../../assets/PNG/additional_illustrations/profile.png')}
+                        />
+                        <View style={styles.chatCardText}>
+                          <Text
+                            style={
+                              styles.userName
+                            }>{`${user.firstName} ${user.lastName}`}</Text>
+                          <Mutation
+                            mutation={ADD_USER_TO_CHAT}
+                            refetchQueries={() => [
+                              {
+                                query: GET_USER_CHATS,
+                                variables: {id: viewer.id},
+                              },
+                            ]}>
+                            {addToUserConversation => (
+                              <TouchableOpacity
+                                style={styles.startChatbutton}
+                                onPress={async () => {
+                                  const chat = await createConversation({
+                                    variables: {
+                                      chatId: chatId,
+                                      userId: user.id,
+                                    },
+                                  });
+                                }}>
+                                <Text style={styles.startChatText}>
+                                  Add Member
+                                </Text>
+                              </TouchableOpacity>
+                            )}
+                          </Mutation>
+                        </View>
+                      </View>
+                    ))}
+                  </ScrollView>
+                  <GradientButton
+                    text="Continue to Chat"
+                    onPress={() => {
+                      navigation.navigate('SingleChat', {chat: chat.id});
+                    }}
+                  />
+                </Fragment>
+              );
+            }}
+          </Query>
         </View>
       </Modal>
     </TouchableOpacity>
