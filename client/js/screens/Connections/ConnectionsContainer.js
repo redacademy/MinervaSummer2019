103eb36ca079e22dd081cb71719e6b2d48aa9fb8
Vincent Wang <a36168120@gmail.com>
2019-09-18 09:35:06 -0700

    add pending request tab on connection page

--- client/js/screens/Connections/ConnectionsContainer.js
+++ client/js/screens/Connections/ConnectionsContainer.js
@@ -11,15 +11,21 @@ export default class ConnectionsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      formToggle: false,
+      selectTopic: 'Suggested',
     };
   }
   static navigationOptions = {
     title: 'Connections',
   };
-  toggleForm = () => {
-    this.setState({formToggle: !this.state.formToggle});
+
+  insertState = topic => {
+    this.setState({selectTopic: topic});
   };
+
+  selectState = () => {
+    return this.state.selectTopic;
+  };
+
   fisherYatesShuffle = a => {
     for (let i = a.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -60,10 +66,11 @@ export default class ConnectionsContainer extends Component {
                 }
                 return (
                   <Connections
-                    toggleForm={this.toggleForm}
                     state={this.state}
                     suggestedUsers={data.allUsers}
                     viewer={context.viewer}
+                    insertState={this.insertState}
+                    selectState={this.selectState}
                   />
                 );
               }}
